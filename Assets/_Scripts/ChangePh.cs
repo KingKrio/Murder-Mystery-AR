d9918d5e71487c88cb1f8927e25e049b32905593
KingKrio <jaycool2@student.ubc.ca>
2024-12-16 22:48:32 -0800

    finished the last of the fixes

--- Assets/_Scripts/ChangePh.cs
+++ Assets/_Scripts/ChangePh.cs
@@ -8,35 +8,47 @@ public class ChangePh : MonoBehaviour
     private int phLvl;
     public TextMeshPro phLabel;
     public GameObject clue;
+    public HoldButton blueHold, redHold;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        phLvl = 3;
+        phLvl = 0;
         phLabel = GetComponent<TextMeshPro>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (phLvl == 11)
+        if (phLvl == 11 && !blueHold.IsPressed && !redHold.IsPressed)
         {
             clue.SetActive(true);
-            phLabel.text = "";
+            phLabel.gameObject.SetActive(false);
+        }
+        else
+        {
+            if (blueHold.IsPressed)
+            {
+                ChangePhLevel(false);
+            }
+            else if (redHold.IsPressed)
+            {
+                ChangePhLevel(true);
+            }
         }
     }
 
-    public void ChangePhLevel(bool incPh)
+    private void ChangePhLevel(bool incPh)
     {
         if (incPh && phLvl < 14)
         {
             phLvl++;
-            phLabel.text = "pH Value: " + phLvl;
         }
         else if (!incPh && phLvl > 0)
         {
             phLvl--;
-            phLabel.text = "pH Value: " + phLvl;
         }
+        phLabel.text = "(pH needed is 11)\npH Value: " + phLvl;
     }
 }
--- Assets/_Scripts/HoldButton.cs
+++ Assets/_Scripts/HoldButton.cs
@@ -1,24 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class HoldButton : MonoBehaviour
+public class HoldButton : MonoBehaviour, IUpdateSelectedHandler, IPointerDownHandler, IPointerUpHandler
 {
-    public bool isPressed;
+    public bool IsPressed => isPressed;
+    private bool isPressed;
 
     // Start is called before the first frame update
     public void OnUpdateSelected(BaseEventData data)
     {
         if (isPressed)
         {
-           
         }
     }
+
     public void OnPointerDown(PointerEventData data)
     {
         isPressed = true;
     }
+
     public void OnPointerUp(PointerEventData data)
     {
         isPressed = false;
